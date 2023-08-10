0ce94cd548034d464210f7d738560cb034728daa
naveen-kk4 <133044450+naveen-kk4@users.noreply.github.com>
2023-08-11 03:39:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
+function findPivot( s , e , arr){
+  let pivot = arr[e];
+  let i = s;
+  for(let j = s;j<e;j++){
+   if(arr[j]<=pivot){
+     let temp = arr[j];
+     arr[j]=arr[i];
+     arr[i]=temp;
+     i++;
+   }
+  }
+  let temp = arr[i];
+  arr[i]=arr[e];
+  arr[e]=temp;
+  return i;
+}
+function quickSort(s,e,arr){
+ if(s>=e)return;
+
+let pivot = findPivot(s,e,arr);
+quickSort(s,pivot-1,arr);
+quickSort(pivot+1,e,arr);
+
+
+}
+
+
+
+
 function calculateMinCost() {
-  //your code here
+  var arr = document.getElementById("rope-lengths").value.split(",").map(function (item) {
+    return parseInt(item, 10);
+});
+  let ans = 0;
+  while(arr.length > 1){
+    quickSort(0,arr.length-1,arr);
+    ans+=arr[0]+arr[1];
+    arr[1]+=arr[0];
+    arr.shift();
+    
+
+  }
+  document.getElementById("result").innerHTML = ans.toString();
   
   
   
